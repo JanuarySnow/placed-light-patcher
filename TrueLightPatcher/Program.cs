6f12ef1e1f82855f15db68fa9f7af21616f8c328
InTheBottle <ronnieceszyk@gmail.com>
2025-08-16 15:21:35 -0500

    Revert "Update Program.cs (#1)"
    This reverts commit 6c90ae8b7ad76afea75497ddd63378bae3a842a3.

--- TrueLightPatcher/Program.cs
+++ TrueLightPatcher/Program.cs
@@ -9,8 +9,8 @@ namespace TrueLightPatcher
 {
     public class Program
     {
-        private static readonly ModKey TrueLight = ModKey.FromFileName("True Light.esm");
-        private static readonly ModKey[] TrueLightAddons = [
+        static ModKey TrueLight { get; } = ModKey.FromFileName("True Light.esm");
+        static ModKey[] TrueLightAddons { get; } = [
             ModKey.FromNameAndExtension("True Light - Creation Club.esp"),
             ModKey.FromNameAndExtension("PL - Default.esp"),
             ModKey.FromNameAndExtension("PL - Dark.esp")
@@ -26,42 +26,38 @@ namespace TrueLightPatcher
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            // Check if True Light.esm is present
-            if (!state.LoadOrder.TryGetValue(TrueLight, out var trueLightListing) || trueLightListing.Mod == null)
+            if (state.LoadOrder.TryGetValue(TrueLight) is not { Mod: not null } TrueLight)
             {
                 Console.Error.WriteLine("'True Light.esm' cannot be found. Make sure you have installed True Light.");
                 return;
-            }
+            };
 
-            // Check for conflicting lighting template plugins
-            if (state.LoadOrder.ContainsKey(ModKey.FromNameAndExtension("TL - Default.esp")) &&
-                state.LoadOrder.ContainsKey(ModKey.FromNameAndExtension("TL - Dark.esp")))
+            if (state.LoadOrder.TryGetValue("TL - Default.esp", out var _) && state.LoadOrder.TryGetValue("TL - Dark.esp", out var _))
             {
                 Console.Error.WriteLine("You are using both 'TL - Default.esp' and 'TL - Dark.esp', please choose only one Lighting Template plugin.");
                 return;
             }
 
-            // Setup list of True Light plugins
-            var trueLightPlugins = new List<ISkyrimModGetter> { trueLightListing.Mod };
+            // Setup list of True Light plugins, adding the Lighting Template override plugins if present
+            var TrueLightPlugins = new List<ISkyrimModGetter> { TrueLight.Mod };
 
-            // Add addons to the list of True Light plugins if found
+            // Add addons to the list of True light plugins if found
             foreach (var modKey in TrueLightAddons)
             {
-                if (state.LoadOrder.TryGetValue(modKey, out var addon) && addon.Mod != null)
-                {
-                    trueLightPlugins.Add(addon.Mod);
-                }
+                if (state.LoadOrder.TryGetValue(modKey) is not { Mod: not null } addon)
+                    continue;
+                TrueLightPlugins.Add(addon.Mod);
             }
 
             var loadOrderLinkCache = state.LoadOrder.ToImmutableLinkCache();
-            var trueLightLinkCache = trueLightPlugins.ToImmutableLinkCache();
+            var TrueLightLinkCache = TrueLightPlugins.ToImmutableLinkCache();
 
-            // Find all interior cells where True Light.esm is not already the winner
+            //Find all interior cells where True Light.esm is not already the winner
             var cellContexts = state.LoadOrder.PriorityOrder.Cell()
                 .WinningContextOverrides(loadOrderLinkCache)
-                .Where(i => i.ModKey != TrueLight)
-                .Where(i => i.Record.Flags.HasFlag(Cell.Flag.IsInteriorCell))
-                .Where(i => !i.Record.MajorFlags.HasFlag(Cell.MajorFlag.Persistent));
+                .Where(static i => i.ModKey != TrueLight)
+                .Where(static i => i.Record.Flags.HasFlag(Cell.Flag.IsInteriorCell))
+                .Where(static i => !i.Record.MajorFlags.HasFlag(Cell.MajorFlag.Persistent));
 
             var cellMask = new Cell.TranslationMask(false)
             {
@@ -71,33 +67,33 @@ namespace TrueLightPatcher
             uint patchedCellCount = 0;
             foreach (var winningCellContext in cellContexts)
             {
-                if (!trueLightLinkCache.TryResolve<ICellGetter>(winningCellContext.Record.FormKey, out var trueLightCellRecord))
+                if (!TrueLightLinkCache.TryResolve<ICellGetter>(winningCellContext.Record.FormKey, out var TrueLightCellRecord))
                     continue;
 
-                if (trueLightCellRecord.Lighting == null)
+                if (TrueLightCellRecord.Lighting == null)
                     continue;
 
-                // If the winning cell record already has the same lighting values as True Light, skip it
-                if (winningCellContext.Record.Equals(trueLightCellRecord, cellMask))
+                // If the winning cell record already has the same lighting values as True Light, skip it.
+                if (winningCellContext.Record.Equals(TrueLightCellRecord, cellMask))
                     continue;
 
-                winningCellContext.GetOrAddAsOverride(state.PatchMod).Lighting = trueLightCellRecord.Lighting.DeepCopy();
+                winningCellContext.GetOrAddAsOverride(state.PatchMod).Lighting = TrueLightCellRecord.Lighting.DeepCopy();
                 patchedCellCount++;
             }
 
             uint patchedLightCount = 0;
             foreach (var winningLightRecord in state.LoadOrder.PriorityOrder.Light().WinningOverrides())
             {
-                if (!trueLightLinkCache.TryResolve<ILightGetter>(winningLightRecord.FormKey, out var trueLightRecord))
+                if (!TrueLightLinkCache.TryResolve<ILightGetter>(winningLightRecord.FormKey, out var TrueLightRecord))
                     continue;
 
                 if (!loadOrderLinkCache.TryResolve<ILightGetter>(winningLightRecord.FormKey, out var originLightRecord, ResolveTarget.Origin))
                     continue;
 
                 // Forward Light records if the winning record is using vanilla values
-                if (winningLightRecord.Equals(originLightRecord) && !winningLightRecord.Equals(trueLightRecord))
+                if (winningLightRecord.Equals(originLightRecord) && !winningLightRecord.Equals(TrueLightRecord))
                 {
-                    state.PatchMod.Lights.DuplicateInAsNewRecord(trueLightRecord);
+                    state.PatchMod.Lights.DuplicateInAsNewRecord(TrueLightRecord);
                     patchedLightCount++;
                 }
             }
